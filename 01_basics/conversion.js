b92c610ddd2ac31ca6120e64eff9a544512fe734
Pradnya-09 <pradnyanakade17@gmail.com>
2025-07-25 04:49:32 +0000

    coversionof datatypes

---
+++ 01_basics/conversion.js
@@ -0,0 +1,35 @@
+// let score =33
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let score= null     //let score=undefined    //let score=true
+// let valueNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+
+
+//"33"=33
+//"33abc"=NaN
+//true=1; false=0
+
+
+// let isLoggedIn =1;
+
+// //let isLoggedIn ="pradnya";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log( booleanIsLoggedIn);
+
+
+// 1=true; 0=false
+// ""=false
+// "pradnya"=true
+
+
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);
